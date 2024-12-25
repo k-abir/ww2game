d583c2111fe84c21b575c802548d709ce0fe23bb
AN <69655447+crotn@users.noreply.github.com>
2024-12-24 17:41:36 -0800

    Update background color to atlantic ocean

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+const var backgroundColor = color(0,92,230);
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(255);
+    background(backgroundColor);
     text("put your p5.js code here",10, frameCount % height);
 }
 
