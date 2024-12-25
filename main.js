2770e1fe3fd968dc55d486433bbaf25fc8e9014d
kabir <143556475+k-abir@users.noreply.github.com>
2024-12-24 19:11:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,23 +7,52 @@ let countries = [
   { name: "Soviet Union", x: 650, y: 200, color: "purple" },
 ];
 
+let startMenu = true;
+let mapOffsetX = 0;
+let mapOffsetY = 0;
+let dragging = false;
+let dragStartX, dragStartY;
+
 function preload() {
   // Preload the map of Europe (replace with a valid hosted image URL)
-  europeMap = loadImage(
-    "EuropeMap.jpg"
-  );
+  europeMap = loadImage("EuropeMap.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop(); // Draw only once unless interaction occurs
+  noLoop(); // Initial render
 }
 
 function draw() {
+  if (startMenu) {
+    drawStartMenu();
+  } else {
+    drawMap();
+  }
+}
+
+function drawStartMenu() {
+  background(50);
+  fill(255);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text("Welcome to the WWII Game!", width / 2, height / 3);
+
+  fill(100, 200, 100);
+  rectMode(CENTER);
+  rect(width / 2, (2 * height) / 3, 200, 60);
+
+  fill(255);
+  textSize(24);
+  text("Start", width / 2, (2 * height) / 3);
+}
+
+function drawMap() {
   background(0, 92, 230); // Set the background to blue
 
-  // Draw the map
-  image(europeMap, 0, 0, windowWidth, windowHeight);
+  push();
+  translate(mapOffsetX, mapOffsetY);
+  image(europeMap, 0, 0, europeMap.width, europeMap.height);
 
   // Highlight participating countries
   for (let country of countries) {
@@ -35,18 +64,51 @@ function draw() {
     textAlign(CENTER);
     text(country.name, country.x, country.y + 30); // Add country label
   }
+  pop();
 }
 
 function mousePressed() {
-  // Check if the mouse is clicked near any country
-  for (let country of countries) {
-    let d = dist(mouseX, mouseY, country.x, country.y);
-    if (d < 25) {
-      alert(`You selected ${country.name}`);
+  if (startMenu) {
+    // Check if Start button is clicked
+    if (
+      mouseX > width / 2 - 100 &&
+      mouseX < width / 2 + 100 &&
+      mouseY > (2 * height) / 3 - 30 &&
+      mouseY < (2 * height) / 3 + 30
+    ) {
+      startMenu = false;
+      loop();
     }
+  } else {
+    dragging = true;
+    dragStartX = mouseX - mapOffsetX;
+    dragStartY = mouseY - mapOffsetY;
+
+    // Check if any country is clicked
+    for (let country of countries) {
+      let d = dist(mouseX - mapOffsetX, mouseY - mapOffsetY, country.x, country.y);
+      if (d < 25) {
+        alert(`You selected ${country.name}`);
+      }
+    }
+  }
+}
+
+function mouseReleased() {
+  dragging = false;
+}
+
+function mouseDragged() {
+  if (!startMenu && dragging) {
+    mapOffsetX = mouseX - dragStartX;
+    mapOffsetY = mouseY - dragStartY;
+    loop(); // Re-render the map while dragging
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  if (!startMenu) {
+    loop();
+  }
 }
